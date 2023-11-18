03693be4929c907d28fa036dbfdff5a2ce553984
omar-sarfraz <omar786089@gmail.com>
2023-11-18 22:17:32 +0500

    [Add] Thumbnail For Anomaly + Code Refactoring

--- routes/api/anomalyLog.js
+++ routes/api/anomalyLog.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const AnomalyLog = require("../../models/AnomalyLog");
 
+const multer = require("multer");
+const fs = require("fs");
+
+const upload = multer({ dest: "/tmp/" });
+
 const router = express.Router();
 
 router.get("/api/anomalyLog", async (req, res) => {
   try {
     const fromDevice = req.body.fromDevice;
-    if (!fromDevice)
-      return res.status(400).json({ message: "Device ID is required" });
+    if (!fromDevice) return res.status(400).json({ message: "Device ID is required" });
+
     const anomalyLogs = await AnomalyLog.find({
       fromDevice: fromDevice,
     }).populate("fromDevice");
+
     if (anomalyLogs.length === 0) return res.status(200).json([]);
     if (anomalyLogs[0].fromDevice.owner != req.user.id)
       return res.status(403).json({
         message: "You are not authorized to access this device's logs",
       });
+
     res.status(200).json(anomalyLogs);
   } catch (error) {
     console.log(error);
@@ -23,8 +30,21 @@ router.get("/api/anomalyLog", async (req, res) => {
   }
 });
 
-router.post("/api/anomalyLog", async (req, res) => {
+router.post("/api/anomalyLog", upload.single("thumbnail"), async (req, res) => {
   try {
+    let anomaly = req.body;
+
+    if (req.file) {
+      let image = fs.readFileSync(req.file.path, "base64");
+
+      // Deleting the file from FileSystem
+      fs.rmSync(req.file.path);
+
+      anomaly.thumbnail = image;
+    } else {
+      return res.status(400).json({ message: "Thumbnail is required!" });
+    }
+
     const anomalyLog = await AnomalyLog.create(req.body);
     res.status(200).json(anomalyLog);
   } catch (error) {
