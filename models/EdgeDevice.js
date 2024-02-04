f37e10ee4a123ef792a5fdd35e0db2f52831ee60
omar-sarfraz <omar786089@gmail.com>
2024-02-04 20:07:38 +0500

    [Change] Edge Device Schema

--- models/EdgeDevice.js
+++ models/EdgeDevice.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+let CameraSchema = mongoose.Schema({
+  cameraName: { type: String, required: true },
+  cameraIP: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  thumbnail: { type: String },
+});
+
 let modelSchema = mongoose.Schema({
   deviceID: { type: String, unique: true, required: true },
   deviceName: String,
@@ -8,7 +16,7 @@ let modelSchema = mongoose.Schema({
     ref: "User",
     default: null,
   },
-  cameras: [],
+  cameras: [CameraSchema],
 });
 
 let Model = mongoose.model("EdgeDevice", modelSchema);
