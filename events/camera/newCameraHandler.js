12b9bfbbaa311a2b7d90d3dedccae4f9d31e163c
Hammad Ali <hammad.a22002@gmail.com>
2024-02-06 23:42:50 +0500

    fix naming issue of camera events

--- events/camera/newCameraHandler.js
+++ events/camera/newCameraHandler.js
@@ -4,7 +4,10 @@ module.exports = (io) => {
   return (info) => {
     console.log("Edge sent one camera", info);
     if (rooms[info.deviceID]?.userSocketId) {
-      io.to(rooms[info.deviceID].userSocketId).emit("camera:discovered:new", info);
+      io.to(rooms[info.deviceID].userSocketId).emit(
+        "cameras:discovered:new",
+        info
+      );
     } else {
       console.log("No user connected!");
     }
