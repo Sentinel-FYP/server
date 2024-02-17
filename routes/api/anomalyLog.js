83f5e25fa35bf088b361fdbe5c5738662e2bebb4
omar-sarfraz <omar786089@gmail.com>
2024-02-18 01:51:43 +0500

    [Add] S3 Bucket Configs

--- routes/api/anomalyLog.js
+++ routes/api/anomalyLog.js
@@ -10,14 +10,16 @@ const upload = multer({ dest: "/tmp/" });
 
 const router = express.Router();
 
-router.get("/api/anomalyLog/:deviceID", async (req, res) => {
+async function getLogs(req, res) {
   try {
     const fromDevice = req.params.deviceID;
     if (!fromDevice) return res.status(400).json({ message: "Device ID is required" });
 
     const anomalyLogs = await AnomalyLog.find({
       fromDevice: fromDevice,
-    }).populate("fromDevice");
+    })
+      .populate("fromDevice")
+      .sort({ createdAt: -1 });
 
     if (anomalyLogs.length === 0) return res.status(200).json([]);
     if (anomalyLogs[0].fromDevice.owner != req.user.id)
@@ -25,19 +27,55 @@ router.get("/api/anomalyLog/:deviceID", async (req, res) => {
         message: "You are not authorized to access this device's logs",
       });
 
-    const logsPromises = anomalyLogs.map(async (log) => ({
-      ...log,
-      videoUri: await createPresignedUrl(log.videoUri),
-    }));
-    const logs = await Promise.all(logsPromises);
-
-    res.status(200).json(logs);
+    res.status(200).json(anomalyLogs);
   } catch (error) {
     console.log(error);
     const schemaErrorMessage = getSchemaError(error);
     res.status(500).send({ message: schemaErrorMessage || "Something went wrong" });
   }
-});
+}
+
+router.get("/api/anomalyLog/:deviceID", getLogs);
+router.get("/api/edgeDevices/:deviceID/anomalyLogs", getLogs);
+
+async function postLog(req, res) {
+  try {
+    let anomaly = req.body;
+    let fromDevice = anomaly.fromDevice;
+
+    let thumbnail = req.files.thumbnail[0];
+    let video = req.files.video[0];
+
+    if (!thumbnail) return res.status(400).json({ message: "Thumbnail is required!" });
+    if (!video) return res.status(400).json({ message: "Video is required!" });
+
+    let videoStream = fs.readFileSync(video.path);
+
+    // Upload Video to S3 Bucket
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fromDevice + "/" + video.originalname,
+      Body: videoStream,
+    };
+
+    const s3UploadResponse = await s3.upload(params).promise();
+    anomaly.videoUri = s3UploadResponse.Key;
+
+    let image = fs.readFileSync(thumbnail.path, "base64");
+    anomaly.thumbnail = image;
+
+    // Deleting the file from FileSystem
+    fs.rmSync(thumbnail.path);
+    fs.rmSync(video.path);
+
+    const anomalyLog = await AnomalyLog.create(req.body);
+    res.status(200).json(anomalyLog);
+  } catch (error) {
+    console.log(error);
+    const schemaErrorMessage = getSchemaError(error);
+    res.status(500).send({ message: schemaErrorMessage || "Something went wrong" });
+  }
+}
 
 // router.post("/api/anomalyLog", upload.single("thumbnail"), async (req, res) => {
 router.post(
@@ -46,43 +84,45 @@ router.post(
     { name: "thumbnail", maxCount: 1 },
     { name: "video", maxCount: 1 },
   ]),
-  async (req, res) => {
-    try {
-      let anomaly = req.body;
-      let fromDevice = anomaly.fromDevice;
-
-      let thumbnail = req.files.thumbnail[0];
-      let video = req.files.video[0];
-
-      if (!thumbnail) return res.status(400).json({ message: "Thumbnail is required!" });
-      if (!video) return res.status(400).json({ message: "Video is required!" });
-
-      // Upload Video to S3 Bucket
-      const params = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: fromDevice + "/" + video.originalname,
-        Body: video.buffer,
-        ContentType: video.mimetype,
-      };
-
-      const s3UploadResponse = await s3.upload(params).promise();
-      anomaly.videoUri = s3UploadResponse.Location;
-
-      let image = fs.readFileSync(thumbnail.path, "base64");
-      anomaly.thumbnail = image;
-
-      // Deleting the file from FileSystem
-      fs.rmSync(thumbnail.path);
-      fs.rmSync(video.path);
-
-      const anomalyLog = await AnomalyLog.create(req.body);
-      res.status(200).json(anomalyLog);
-    } catch (error) {
-      console.log(error);
-      const schemaErrorMessage = getSchemaError(error);
-      res.status(500).send({ message: schemaErrorMessage || "Something went wrong" });
-    }
-  }
+  postLog
 );
+
+router.post(
+  "/api/anomalyLogs",
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "video", maxCount: 1 },
+  ]),
+  postLog
+);
+
+router.get("/api/anomalyLogs/:anomalyLogID", async (req, res) => {
+  try {
+    const anomalyLogID = req.params.anomalyLogID;
+    if (!anomalyLogID) return res.status(400).json({ message: "Anomaly Log ID is required" });
+
+    let anomalyLog = await AnomalyLog.findOne({
+      _id: anomalyLogID,
+    })
+      .populate("fromDevice")
+      .lean();
+
+    if (!anomalyLog)
+      return res.status(404).json({ message: "Anomaly Log with this ID does not exist!" });
+    if (anomalyLog.fromDevice.owner != req.user.id)
+      return res.status(403).json({
+        message: "You are not authorized to access this device's logs",
+      });
+
+    let videoUri = await createPresignedUrl(anomalyLog.videoUri);
+    anomalyLog = { ...anomalyLog, videoUri };
+
+    res.status(200).json(anomalyLog);
+  } catch (error) {
+    console.log(error);
+    const schemaErrorMessage = getSchemaError(error);
+    res.status(500).send({ message: schemaErrorMessage || "Something went wrong" });
+  }
+});
 
 module.exports = router;
